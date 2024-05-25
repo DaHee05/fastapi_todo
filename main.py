7d5b4af953b9e71fc196882d92becae472d6637f
DaHee05 <dbrud212gh@naver.com>
2024-05-25 18:39:30 +0900

    add delete todo list

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from todo import todo_router # added
 import uvicorn
 
 app = FastAPI()
+
+origins = ["http://127.0.0.1:5500", "http://127.0.0.1:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def welcome() -> dict:
